7802a594ba8d62d879bc63a4f67056327fe05842
Geoffrey Sessums <geoffreysessums@gmail.com>
2018-06-27 16:15:48 -0500

    Program completed

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # Queues
 Written in C, Queues contains a simple implementation of a ring-buffered queue array.
+#### Description:
+## Installation:
+Clone the repository wherever you like (e.g. ~/Projects/Queues):
+```bash
+git clone https://github.com/gsessums/Queues.git
+```
+## Usage
+Use the included Makefile:
+```bash
+make
+```
+To execute:
+```
+./queuearray
+```
+
+## Credits
+Author: [Geoffrey Sessums](http://www.geoffreysessums.com)
+
+## License
+MIT License
+
+Copyright (c) 2018 Geoffrey Sessums
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- queuearray.c
+++ queuearray.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // max size of ring-buffered queue array
-#define MAX_SIZE 10
+#define MAX_SIZE 9 
 #define QUEUE_ARRAY_SIZE (MAX_SIZE + 1)
 
 // declare a character array of size 10
@@ -16,12 +16,12 @@ int tail = 0;
 // Queue Functions
 void Enqueue(char element)
 {
-    if (isFull()) 
+    if (IsFull()) 
     {
         printf("Error: Could NOT insert element, the queue is FULL\n\n");
     } else {
         queue[tail] = element;
-        tail++;
+        tail = (tail + 1) % QUEUE_ARRAY_SIZE;
     }
 } // end of Enqueue
 
@@ -29,29 +29,117 @@ char Dequeue()
 {
     char element;
 
-    if (isEmpty())
+    if (IsEmpty())
     {
         printf("Error: Could NOT remove element, the queue is EMPTY\n\n");
     } else {
         element = queue[head];
+        head = (head + 1) % QUEUE_ARRAY_SIZE;
         return element;
     }
 } // end of Dequeue
 
-int isFull()
+int IsFull()
+{
+    if (head == (tail + 1) % QUEUE_ARRAY_SIZE)
+    {
+        return 1;
+    }
+    return 0;
+} // end of isFull
+
+int IsEmpty()
 {
     if (head == tail)
     {
-
+        return 1;
     }
-} // end of isFull
-
-int isEmpty()
-{
-
+    return 0;
 } // end of is Empty
 
-void main(void) 
+int main(void) 
 {
+    char element;
 
+    // Brief program description
+    printf("\n%s\n\n%s\n%s\n\n"
+        , "Program: queuearray.c by Geoffrey Sessums"
+        , "This program demonstrates a simple queue implementation using an "
+        , "array of fixed size (10).");
+    
+    // Check if queue is empty
+    printf("Attempt to dequeue from empty queue, then print head & tail.\n");
+    element = Dequeue();
+    printf("Queue status: head = %d, tail = %d\n\n", head, tail);
+
+    // Enqueue 5 elements, then print head and tail values
+    printf("Enqueue A\n");
+    Enqueue('A');
+    printf("Enqueue B\n");
+    Enqueue('B');
+    printf("Enqueue C\n");
+    Enqueue('C');
+    printf("Enqueue D\n");
+    Enqueue('D');
+    printf("Enqueue E\n");
+    Enqueue('E');
+    printf("\nQueue status: head = %d, tail = %d\n\n", head, tail);
+
+    // Dequeue 1 element, then print head and tail values
+    printf("%s%c\n%s%d%s%d\n\n"
+        , "Dequeue ", Dequeue(), "\nQueue status: head = ", head, " tail = ", tail);
+
+   // Alternate enqueue and dequeue operations until element J
+    printf("Enqueue F\n");
+    Enqueue('F');
+    printf("%s%c\n%s%d%s%d\n\n"
+        , "Dequeue ", Dequeue(), "\nQueue status: head = ", head, " tail = ", tail);
+    printf("Enqueue G\n");
+    Enqueue('G');
+    printf("%s%c\n%s%d%s%d\n\n"
+        , "Dequeue ", Dequeue(), "\nQueue status: head = ", head, " tail = ", tail);
+    printf("Enqueue H\n");
+    Enqueue('H');
+    printf("%s%c\n%s%d%s%d\n\n"
+        , "Dequeue ", Dequeue(), "\nQueue status: head = ", head, " tail = ", tail);
+    printf("Enqueue I\n");
+    Enqueue('I');
+    printf("%s%c\n%s%d%s%d\n\n"
+        , "Dequeue ", Dequeue(), "\nQueue status: head = ", head, " tail = ", tail);
+    printf("Enqueue J\n");
+    Enqueue('J');
+
+    printf("%s%d%s%d\n\n"
+        , "\nQueue status: head = ", head, " tail = ", tail);
+    
+    printf("%s%s%s\n\n"
+        , "At this point, the tail is beyond the end of our queue array."
+        , " Notice how the tail can \"wrap around\" from the end of the queue"
+        , " array to the beginning position.");
+
+    // Enqueue 1 element, then print the queue status
+    printf("Enqueue K\n");
+    Enqueue('K');
+    printf("\nQueue status: head = %d, tail = %d\n\n", head, tail);
+
+    printf("Enqueue L\n");
+    Enqueue('L');
+    printf("Enqueue M\n");
+    Enqueue('M');
+    printf("Enqueue N\n");
+    Enqueue('N');
+    printf("\nQueue status: head = %d, tail = %d\n\n", head, tail);
+
+    // Check if queue is full 
+    printf("Attempt to enqueue 'O' to a full queue, then print queue status.\n");
+    Enqueue('O');
+    printf("Queue status: head = %d, tail = %d\n\n", head, tail);
+
+    // Print the entire contents of the queue array
+    printf("Dequeue and print the entire contents of the queue array.\n");
+    printf("Queue: ");
+    while (!IsEmpty()) {
+        printf("%c ", Dequeue());
+    }
+    printf("\nQueue status: head = %d, tail = %d\n\n", head, tail);
 } // end of main
